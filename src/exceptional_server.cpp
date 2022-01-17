c3f1d4396f1606a040f8842cf6e2d7405814ea6d
AlexICMT <alexicmt1212@gmail.com>
2022-01-17 11:32:21 +0300

    Added Exceptional Server

--- src/exceptional_server.cpp
+++ src/exceptional_server.cpp
@@ -13,21 +13,34 @@
 #include <vector>
 #include <cmath>
 using namespace std;
+long long A, B;
 
 class Server {
 private:
 	static int load;
+
 public:
 	static int compute(long long A, long long B) {
 		load += 1;
 		if(A < 0) {
 			throw std::invalid_argument("A is negative");
 		}
+
+		cout <<"before allocating of v" << '\n';
+
 		vector<int> v(A, 0);
+
+		for (int number : v)
+		cout <<"v[]" <<  number << '\n';
+
 		int real = -1, cmplx = sqrt(-1);
 		if(B == 0) throw 0;
 		real = (A/B)*real;
 		int ans = v.at(B);
+
+		cout <<"ans" <<  ans << '\n';
+		cout <<"result" <<  (real + A - B*ans) << '\n';
+
 		return real + A - B*ans;
 	}
 	static int getLoad() {
@@ -39,33 +52,43 @@ int Server::load = 0;
 int main() {
 	int T; cin >> T;
 	while(T--) {
-		long long A, B;
+		//long long A, B;
 		cin >> A >> B;
 
 		/* Enter your code here. */
 		try
 		{
-			if (A > LLONG_MAX || B > LLONG_MAX)
-			{
-				throw 1;
-			}
-			if (Server::compute(A, B) > LLONG_MAX);
-			{
-				throw std::invalid_argument("Not enough memory");
-			}
-
+			if ((T+1) < 1 || (T+1) > 1000)
+				return -2;
+			if (cin.fail())
+				throw -1;
+			 cout << Server::compute(A,B) << endl;
 		}
-		catch (std::invalid_argument a)
+		catch (invalid_argument& a)
 		{
-			cout << a.what() << '\n';
+			cout << "Exception: " << a.what() << '\n';
 		}
-		catch (int a)
+		catch (int& a)
 		{
-			cout << "Exception:" << a << '\n';
+			cout << "Exception: " << a << '\n';
+		}
+		catch (bad_alloc& a)
+		{
+			cout << "Not enough memory" << '\n';
+		}
+		catch (const exception& a)
+		{
+			cout << "Exception: " << a.what() << '\n';
+			//cout << "Other Exception." << '\n';
+		}
+		catch (...)
+		{
+			cout << "Other Exception." << '\n';
 		}
 
 	}
-	//cout << sizeof(long long);
+	cout <<"A=" << A << '\n';
+	cout <<"B=" << B << '\n';
 
 	cout << Server::getLoad() << endl;
 	return 0;
