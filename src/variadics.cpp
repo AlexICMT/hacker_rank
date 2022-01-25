4dd8cceb43e4be89c492cd8313aac83f78ee62e6
AlexICMT <alexicmt1212@gmail.com>
2022-01-25 16:30:37 +0300

    Added variadic task, started bit_array...

--- src/variadics.cpp
+++ src/variadics.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <variant>
 #include <tuple>
+#include <vector>
+#include <list>
+#include <map>
 //using namespace std;
 
 template <typename T>
@@ -37,10 +40,103 @@ double power_sum(T t, Rest... rest)
 	return t + power_sum(square(rest)...);
 }
 
-//int n_main_6
+template<typename T> bool pair_comparer(T a, T b) { return a == b; }
 
-int main()
+template<typename T> bool pair_comparer(T a) { return false; }
+
+template<typename T, typename... Args>
+bool pair_comparer(T a, T b, Args... args)
 {
+	std::cout << __PRETTY_FUNCTION__ << "\n";
+	return a == b && pair_comparer(args...);
+}
+
+//The instant of the prime tuple.
+template<class... Ts> struct tuple {};
+
+template<class T, class... Ts>
+struct tuple<T, Ts...> : tuple<Ts...>
+{
+	tuple(T t, Ts... ts) : tuple<Ts...>(ts...), tail(t) {std::cout << __PRETTY_FUNCTION__ << "\n";}
+	T tail;
+};
+
+template<size_t, class> struct elem_type_holder;
+
+template<class T, class... Ts>
+struct elem_type_holder<0, tuple<T, Ts...>>
+{
+	typedef T type;
+};
+
+template<size_t k, class T, class... Ts>
+struct elem_type_holder<k, tuple<T, Ts...>>
+{
+	typedef typename elem_type_holder<k - 1, tuple<Ts...>>::type type;
+};
+
+template <size_t k, class... Ts>
+typename std::enable_if<
+    k == 0, typename elem_type_holder<0, tuple<Ts...>>::type&>::type
+get(tuple<Ts...>& t) {
+  return t.tail;
+}
+
+template <size_t k, class T, class... Ts>
+typename std::enable_if<
+    k != 0, typename elem_type_holder<k, tuple<T, Ts...>>::type&>::type
+get(tuple<T, Ts...>& t) {
+  tuple<Ts...>& base = t;
+  return get<k - 1>(base);
+}
+
+template <template <typename, typename> class ContainerType,
+          typename ValueType,
+          typename AllocType>
+void print_container(const ContainerType<ValueType, AllocType>& c) {
+  for (const auto& v : c) {
+    std::cout << v << ' ';
+  }
+  std::cout << '\n';
+}
+
+template <template <typename, typename...> class ContainerType,
+          typename ValueType, typename... Args>
+void print_container(const ContainerType<ValueType, Args...>& c) {
+  for (const auto& v : c) {
+    std::cout << v << ' ';
+  }
+  std::cout << '\n';
+}
+
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& out, const std::pair<T, U>& p) {
+  out << "[" << p.first << ", " << p.second << "]";
+  return out;
+}
+
+int n_main_6()
+
+//int main()
+{
+	std::map<std::string, int> msi{{"foo", 42}, {"bar", 81}, {"bazzo", 4}};
+	print_container(msi);
+
+	std::vector<double> vd{3.14, 8.1, 3.2, 1.0};
+	print_container(vd);
+
+	std::list<int> li{1, 2, 3, 5};
+	print_container(li);
+
+	tuple<double, uint64_t, const char*> t1(12.2, 42, "big");
+	std::cout << "0th elem is " << get<0>(t1) << "\n";
+	std::cout << "1th elem is " << get<1>(t1) << "\n";
+	std::cout << "2th elem is " << get<2>(t1) << "\n";
+
+	get<1>(t1) = 103;
+	std::cout << "1th elem is " << get<1>(t1) << "\n";
+
+	std::cout << pair_comparer (1.5, 1.5, 2, 2, 6, 6, 7) << std::endl;
 	std::cout << sum(1, 2, 3, 8, 7);
     return 0;
 }
--- src/variadics_tsk.cpp
+++ src/variadics_tsk.cpp
@@ -6,26 +6,72 @@
  *      Author: alexicmt
  */
 
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
+// base case
+void doPrint(std::ostream& out) {}
 
-// Enter your code for reversed_binary_value<bool...>()
+template <typename T>
+void doPrint(std::ostream& out, T t)
+{
+    out << ' ' << t;
+}
+
+template <typename T, typename U, typename... Args>
+void doPrint(std::ostream& out, T t, U u, Args... args)
+{
+    out << ' ' << t << ',';
+    doPrint(out, u, args...);
+}
+
+vector<bool>v {};
+int counter {}, result {};
+
+template<bool t>
+int reversed_binary_value()
+{
+
+	std::cout << __PRETTY_FUNCTION__ << "\n";
+	return t;
+}
+
+template<typename T>
+int reversed_binary_value(T t)
+{
+	std::cout << __PRETTY_FUNCTION__ << "\n";
+	return t;
+}
+
+template<typename T, typename... Rest>
+int reversed_binary_value(T t, Rest... rest)
+{
+	std::cout << __PRETTY_FUNCTION__ << "\n";
+	return ((t << 1) + reversed_binary_value(rest...));
+}
+
 template<bool N, bool... S> int reversed_binary_value()
 {
 	//bool&& rv = N;
+	int size = sizeof...(S);
+	std::cout << "size= " << size << "\n";
+	//if(!(static_cast<int>(v.size()) == 0))
 
-	cout << N;
-	return sizeof... (S);
+	v.push_back(N);
+	std::cout << __PRETTY_FUNCTION__ << "\n";
+	//return ((N<<(counter - 1)) + reversed_binary_value(S...));
+	result += N << (counter);
+	counter++;
+	return N + reversed_binary_value(S...);
 }
 
-
-template <int n, bool...digits>
+template <int n, bool... digits>
 struct CheckValues {
   	static void check(int x, int y)
   	{
     	CheckValues<n-1, 0, digits...>::check(x, y);
-
     	CheckValues<n-1, 1, digits...>::check(x, y);
   	}
 };
@@ -34,21 +80,32 @@ template <bool...digits>
 struct CheckValues<0, digits...> {
   	static void check(int x, int y)
   	{
-    	int z = reversed_binary_value<digits...>();
-    	//std::cout << (z+64*y==x);
+  		std::cout << __PRETTY_FUNCTION__ << "\n";
+  		int z = reversed_binary_value<digits...>();
+    	cout << z << endl;
+    	std::cout << (z+64*y==x);
   	}
 };
 
 int no_main_7()
 //int main()
 {
-  	int t; std::cin >> t;
+int t; std::cin >> t;
 
   	for (int i=0; i!=t; ++i) {
 		int x, y;
     	cin >> x >> y;
     	CheckValues<6>::check(x, y);
     	cout << "\n";
+        std::cout << "v = { ";
+        for (int n : v) {
+            std::cout << n << ", ";
+        }
+        std::cout << "}; \n";
   	}
+  	cout << v.size();
+  	cout << counter;
+  	cout << result;
   	return 0;
 }
+
